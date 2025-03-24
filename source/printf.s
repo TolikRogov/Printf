6fbca31511fc4212743584a8c2e4460ebad491b9
TolikRogov <tolik_rogov@bk.ru>
2025-03-25 01:46:12 +0300

    to 10

--- source/printf.s
+++ source/printf.s
@@ -11,22 +11,26 @@ section .data										;start of data segment
 String 			db "My string: %s %x %d%%%c", 10, 0
 SpecifierS 		db "I love", 0
 SpecifierX		dq 3802
-SpecifierD		dq 100
+SpecifierD		dd 100
 SpecifierC		db '!'
 Stdout 			equ 0x01							;descriptor of stdout
 buffer_size 	equ 128								;size of buffer
+trans_buff_size	equ 64								;size of translator buffer
 
 section .rodata
 jump_table 	dq 	_b_
 			dq	_c_
 			dq 	_d_
-			times 's' - 'd' - 1 dq _default_
+			times 'o' - 'd' - 1 dq _default_
+			dq 	_o_
+			times 's' - 'o' - 1 dq _default_
 			dq	_s_
 			times 'x' - 's' - 1 dq _default_
 			dq	_x_
 
 section .bss										;start non-prog segment
 Buffer 			resb buffer_size					;Init buffer
+trans_buffer 	resb trans_buff_size				;Init trans buffer
 
 section .text										;start of code segment
 main:												;input pointer
@@ -49,11 +53,11 @@ main:												;input pointer
 
 	;cdecl
 	mov rbp, rsp
-	push SpecifierC									;fifth argument
-	push SpecifierD									;fourth argument
-	push SpecifierX									;third argument
-	push SpecifierS									;second argument
-	push String										;first argument
+	mov rdi, String									;format string
+	mov rsi, SpecifierS								;first argument
+	mov rdx, [SpecifierX]							;second argument
+	mov ecx, [SpecifierD]							;third argument
+	mov r8, [SpecifierC]							;fourth argument
 	call my_printf									;function call
 	mov rsp, rbp
 
@@ -61,9 +65,9 @@ main:												;input pointer
 	pop r15
 	pop r14
 	pop r13
-	pop rdx
-	pop rcx
-	pop rax
+	pop r12
+	pop rbx
+	pop rbp
 
 	call exit
 
@@ -75,8 +79,17 @@ main:												;input pointer
 ;	Destroy:	None
 ;==============================================================================
 my_printf:											;printf(char* string, ...)
-	mov rsi, [rsp + 8]								;first format string
-	mov r12, 1										;r12 - current amount of arguments
+	pop r10											;returning address
+	push r9
+	push r8
+	push rcx
+	push rdx
+	push rsi
+	push rdi
+	mov r11, rsp									;start of parameters in stack
+
+	mov rsi, [r11]									;first format string
+	add r11, 8
 
 	mov rdi, Buffer									;copy destination
 	copy_to_buffer:									;<------------------------------|
@@ -93,6 +106,7 @@ my_printf:											;printf(char* string, ...)
 													;									|
 	end_printf:										;<----------------------------------|
 	call flush_buffer								;flush the buffer
+	push r10
 	ret
 ;==============================================================================
 
@@ -183,43 +197,19 @@ GetArg:
 	sub al, 'b'										;al -= 'b'
 	cmp al, 'x' - 'b'								;if (al == 'x' - 'b') zf = 1
 	ja _default_									;if (>) goto _default_
-	add r12, 1										;increase amount of arguments
 	and rax, 0xFF									;rax &= 0xFF
 	shl rax, 3										;rax *= 8
 	add rax, jump_table								;rax += &jump_table
 
-	mov rbx, r12									;rbx = args cnt
-	shl rbx, 3										;rbx *= 8
-	add rbx, rsp									;rbx += rsp
 	push rsi										;save rsi
-	mov rsi, [rbx]									;rsi = new arg
+	mov rsi, [r11]									;rsi = new arg
+	add r11, 8
 
 	jmp [rax]										;goto jump_table + al * 8
-
-	_b_:
-		pop rsi
-		jmp _default_
-
-	_c_:
-		lodsb
-		call check_buffer
-		pop rsi
-		jmp _default_
-
-	_d_:
-		pop rsi
-		jmp _default_
-
-	_x_:
-		pop rsi
-		jmp _default_
-
-	_default_:
-		jmp copy_to_buffer
 ;==============================================================================
 
 ;==============================================================================
-;	Handler of %s parameter
+;	Handler of parameters
 ;	Entry:		None
 ;	Exit:		None
 ;	Destroy:	None
@@ -230,6 +220,63 @@ _s_:
 		lodsb
 		call check_buffer
 	loop str_to_buffer
+	pop rsi
+	jmp _default_
+
+_c_:
+	mov rax, rsi
+	call check_buffer
+	pop rsi
+	jmp _default_
+
+_b_:
+	pop rsi
+	jmp _default_
+
+_o_:
+	pop rsi
+	jmp _default_
+
+_x_:
+	pop rsi
+	jmp _default_
+
+_default_:
+	jmp copy_to_buffer
+;==============================================================================
+
+;==============================================================================
+;	Handler %d parameter
+;	Entry:		None
+;	Exit:		None
+;	Destroy:	None
+;==============================================================================
+_d_:
+	push rdi
+	mov rdi, trans_buffer
+	mov rcx, 10
+	mov rax, rsi
+	division:
+		cqo
+		div rcx
+		add rdx, '0'
+		mov [rdi], rdx
+		inc rdi
+		cmp rax, 0
+	ja division
+	mov rsi, rdi
+	pop rdi
+
+	mov rcx, rsi
+	sub rcx, trans_buffer
+	inc rcx
+
+	put_to_buffer:
+		movsb
+		inc rdi
+		sub rsi, 2
+	loop put_to_buffer
+
 	pop rsi
 	jmp _default_
 ;==============================================================================
