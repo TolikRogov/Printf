c8e7585dc37346ec0b8b5cbcf6ac1385e34ed059
TolikRogov <tolik_rogov@bk.ru>
2025-03-24 12:10:28 +0300

    s and c parameters

--- source/printf.s
+++ source/printf.s
@@ -20,9 +20,9 @@ section .rodata
 jump_table 	dq 	_b_
 			dq	_c_
 			dq 	_d_
-			times 's' - 'd' dq _default_
+			times 's' - 'd' - 1 dq _default_
 			dq	_s_
-			times 'x' - 's' dq _default_
+			times 'x' - 's' - 1 dq _default_
 			dq	_x_
 
 section .bss										;start non-prog segment
@@ -49,6 +49,9 @@ main:												;input pointer
 
 	;cdecl
 	mov rbp, rsp
+	push SpecifierC									;fifth argument
+	push SpecifierD									;fourth argument
+	push SpecifierX									;third argument
 	push SpecifierS									;second argument
 	push String										;first argument
 	call my_printf									;function call
@@ -72,7 +75,8 @@ main:												;input pointer
 ;	Destroy:	None
 ;==============================================================================
 my_printf:											;printf(char* string, ...)
-	mov rsi, [rsp + 8]								;&string
+	mov rsi, [rsp + 8]								;first format string
+	mov r12, 1										;r12 - current amount of arguments
 
 	mov rdi, Buffer									;copy destination
 	copy_to_buffer:									;<------------------------------|
@@ -81,7 +85,7 @@ my_printf:											;printf(char* string, ...)
 		cmp al, '%'									;if (al == '%') zf = 1			|
 		je GetArg									;if (zf == 1) goto GetArg		|
 													;								|
-		cmp al, 0									;if (al == '$') zf = 1			|
+		cmp al, 0									;if (al == 0) zf = 1			|
  		je end_printf								;if (zf == 1) goto end_printf---|---|
 													;								|	|
 		call check_buffer							;flush buffer before put symbol?|	|
@@ -123,6 +127,9 @@ check_buffer:
 ;==============================================================================
 flush_buffer:
 	push rsi										;save rsi
+	push rdx										;save rdx
+	push rax										;save rax
+	push rcx										;save rcx
 	mov rsi, Buffer									;rsi = &Buffer
 	mov rdx, buffer_size							;rdx = buffer len
 	mov rax, 0x01									;write
@@ -133,8 +140,29 @@ flush_buffer:
 	mov rdi, Buffer									;rdi = buffer start
 	xor rax, rax									;rax = 0
 	rep stosb										;while (rcx--) stosb
+	pop rcx											;return rcx
+	pop rax											;return rax
+	pop rdx											;return rdx
 	pop rsi											;return rsi
 	ret
+;==============================================================================
+;	Calculate string length
+;	Entry:		RSI - address of string
+;	Exit:		RCX - string length
+;	Destroy:	RCX, AL
+;==============================================================================
+strlen:												;strlen(rsi)
+	push rsi										;save rsi
+	xor rcx, rcx									;rcx = 0
+	count:											;<----------------------------------|
+		lodsb										;mov al, ds:[rsi] / inc rsi			|
+		cmp al, 0									;if (al == 0) zf = 1				|
+		je end_count								;if (zf == 1) goto end_count----|	|
+		add rcx, 1									;rcx += 2						|	|
+	jmp count										;-------------------------------|---|
+	end_count:										;<------------------------------|
+	pop rsi											;back rsi | string start
+ret
 ;==============================================================================
 
 ;==============================================================================
@@ -155,26 +183,53 @@ GetArg:
 	sub al, 'b'										;al -= 'b'
 	cmp al, 'x' - 'b'								;if (al == 'x' - 'b') zf = 1
 	ja _default_									;if (>) goto _default_
+	add r12, 1										;increase amount of arguments
 	and rax, 0xFF									;rax &= 0xFF
 	shl rax, 3										;rax *= 8
 	add rax, jump_table								;rax += &jump_table
+
+	mov rbx, r12									;rbx = args cnt
+	shl rbx, 3										;rbx *= 8
+	add rbx, rsp									;rbx += rsp
+	push rsi										;save rsi
+	mov rsi, [rbx]									;rsi = new arg
+
 	jmp [rax]										;goto jump_table + al * 8
 
 	_b_:
+		pop rsi
 		jmp _default_
 
 	_c_:
+		lodsb
+		call check_buffer
+		pop rsi
 		jmp _default_
 
 	_d_:
-		jmp _default_
-
-	_s_:
+		pop rsi
 		jmp _default_
 
 	_x_:
+		pop rsi
 		jmp _default_
 
 	_default_:
 		jmp copy_to_buffer
+;==============================================================================
+
+;==============================================================================
+;	Handler of %s parameter
+;	Entry:		None
+;	Exit:		None
+;	Destroy:	None
+;==============================================================================
+_s_:
+	call strlen
+	str_to_buffer:
+		lodsb
+		call check_buffer
+	loop str_to_buffer
+	pop rsi
+	jmp _default_
 ;==============================================================================
